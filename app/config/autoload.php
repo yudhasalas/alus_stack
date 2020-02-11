124fe93d7b2f85a0983e6f83021053d65d9aa444
MaulanaR <maulana.code@gmail.com>
2020-02-11 15:01:08 +0700

    remove autoload notif

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 //$autoload['libraries'] = array();
-$autoload['libraries'] = array('database','session','form_validation','pagination','alus_auth','push_notif');
+$autoload['libraries'] = array('database','session','form_validation','pagination','alus_auth');
 
 
 /*
